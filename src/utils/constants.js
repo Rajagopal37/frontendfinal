049c09fcacf75e5f9ecd2bbbb5e7c9b3453a4469
Rajagopal37 <trajagopal37@gmail.com>
2024-10-14 06:22:28 +0530

    base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,9 @@
 // export const BASE_URL = 'http://localhost:8000'
-// export const BASE_URL = 'https://task-backend-3-9oq7.onrender.com'
+export const BASE_URL = 'https://task-backend-3-9oq7.onrender.com'
 
 // export const BASE_URL = process.env.NODE_ENV === 'development'
 //   ? 'http://localhost:8000'
 //   : 'https://task-backend-3-9oq7.onrender.com';
 
 
-  export const BASE_URL = import.meta.env.VITE_BASE_URL;
+//   export const BASE_URL = import.meta.env.VITE_BASE_URL;
